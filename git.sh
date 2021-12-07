09d602396c76fffb0dac146c603114d6f37290b2
vinay-14298 <vtu14298@veltech.edu.in>
2021-12-07 15:21:36 +0530

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# Express-App-B" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vinay-14298/Express-App-B.git
+git push -u origin main
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// to require lib module
+import React,{Component} from 'react'
+import ReactDom from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import JSON from './users.json';
+import NewsList from './components/NewsList';
+import axios from "axios";
+
+// Logical and html
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            news:JSON,
+            filtered:JSON
+        }
+    }
+    componentDidMount() {
+        axios.get("/users.json").then((response) => {
+          this.setState({ news: response.data,filtered : response.data });
+        });
+      }
+    filterNews(keyword){
+        const output = this.state.news.filter((data)=>{
+            return data.title.indexOf(keyword)> -1;
+        })
+
+        this.setState({filtered:output})
+    }
+
+
+    render(){
+        return(
+            <div>
+                <Header newsSearch={(userText)=>{this.filterNews(userText)}}/>
+                <hr/>
+                <NewsList datalist={this.state.filtered}/>
+            </div>
+        )
+    }
 }
 
 export default App;
